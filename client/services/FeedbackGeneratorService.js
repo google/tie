a26a197db7d6391650e31d7616de183b91b2bae7
Sean Lip <sll@google.com>
2017-03-10 18:38:11 -0800

    Address review comment.

--- client/services/FeedbackGeneratorService.js
+++ client/services/FeedbackGeneratorService.js
@@ -46,6 +46,8 @@ tie.factory('FeedbackGeneratorService', [
         }
         return '{' + humanReadableKeyValuePairs.join(', ') + '}';
       } else {
+        console.error(
+          'Could not make the following object human-readable: ', jsVariable);
         return '[UNKNOWN OBJECT]';
       }
     };
