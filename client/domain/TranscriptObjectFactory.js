40bc6c3e088a80f2402010f8810a94447fb8546b
Eric Yurko <eric.yurko@gmail.com>
2017-03-16 16:59:36 -0700

    TranscriptObjectFactory got left out somehow, too.

---
+++ client/domain/TranscriptObjectFactory.js
@@ -0,0 +1,50 @@
+// Copyright 2017 The TIE Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Factory for creating a transcript of a student's TIE 
+ * session.
+ */
+
+tie.factory('TranscriptObjectFactory', [
+  function() {
+    var Transcript = function() {
+      this._snapshots = [];
+      this._events = [];
+    };
+
+    // Instance methods.
+    Transcript.prototype.getPreviousSnapshot = function() {
+      return this._snapshots[this._snapshots.length - 1];
+    };
+
+    Transcript.prototype.recordSnapshot = function(snapshot) {
+      this._snapshots.push(snapshot);
+      return this._snapshots.length;
+    };
+
+    // TODO(eyurko): Probably not needed. Remove in a subsequent PR.
+    Transcript.prototype.recordEvent = function(event) {
+      this._events.push(event);
+      return this._events.length;
+    };
+
+    // Static class methods.
+    Transcript.create = function() {
+      return new Transcript();
+    };
+
+    return Transcript;
+  }
+]);
