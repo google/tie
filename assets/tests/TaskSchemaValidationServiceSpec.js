69cf16412d42473696d731e1c06cbebaef4ad7de
Eric Yurko <eric.yurko@gmail.com>
2017-03-29 16:01:47 -0700

    AdFixed linter errors.

--- assets/tests/TaskSchemaValidationServiceSpec.js
+++ assets/tests/TaskSchemaValidationServiceSpec.js
@@ -18,7 +18,8 @@
 
 describe('TaskSchemaValidationService', function() {
   var QuestionDataService;
-  var TaskSchemaValidationService;
+  // TaskSchemaValidationService needs a shorter name.
+  var Tsvs;
   var questions = [];
   // Should contain all question IDs.
   // TODO(eyurko): Figure out a way to dynamically check to make sure
@@ -31,10 +32,8 @@ describe('TaskSchemaValidationService', function() {
   beforeEach(inject(function($injector) {
     // Used for testing the validator. Values will be inserted during the tests
     // so that we don't have to redefine the dict every time.
-    QuestionDataService = $injector.get(
-      'QuestionDataService');
-    TaskSchemaValidationService = $injector.get(
-      'TaskSchemaValidationService');
+    QuestionDataService = $injector.get('QuestionDataService');
+    Tsvs = $injector.get('TaskSchemaValidationService');
     QuestionDataService.initCurrentQuestionSet('all');
     questions = [];
     questionIds.forEach(function(item) {
@@ -46,7 +45,12 @@ describe('TaskSchemaValidationService', function() {
     questions.forEach(function(item) {
       var tasks = item.getTasks();
       for (var i = 0; i < tasks.length; i++) {
-        assertTrue(func(tasks[i]), [item.getTitle(), message, i].join(''));
+        if (message) {
+          expect(func(tasks[i])).toBe(true,
+            [item.getTitle(), message, i].join(''));
+        } else {
+          expect(func(tasks[i])).toBe(true);
+        }
       }
     });
   };
@@ -55,7 +59,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that a task has nonempty instructions',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyInstructionsAreNotEmpty,
+          Tsvs.verifyInstructionsAreNotEmpty,
           ' has an empty instruction for the task at index ');
       });
   });
@@ -64,7 +68,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that a task has an array of string instructions',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyInstructionsIsArrayOfStrings,
+          Tsvs.verifyInstructionsIsArrayOfStrings,
           ' does not have a string instructions array for the task at index ');
       });
   });
@@ -73,7 +77,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that a task has a string mainFunctionName',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyMainFunctionNameIsString,
+          Tsvs.verifyMainFunctionNameIsString,
           ' does not have a string mainFunctionName for the task at index ');
       });
   });
@@ -89,18 +93,19 @@ describe('TaskSchemaValidationService', function() {
           var tasks = item.getTasks();
           for (var i = 0; i < tasks.length; i++) {
             // Can't use assertTrueForAllTasks here; need starterCode.
-            assertTrue(TaskSchemaValidationService.verifyMainFunctionNameAppearsInStarterCode(
-              tasks[i], starterCode), [item.getTitle(), message, i].join(''));
+            expect(Tsvs.verifyMainFunctionNameAppearsInStarterCode(
+              tasks[i], starterCode)).toBe(true,
+                [item.getTitle(), message, i].join(''));
           }
         });
       });
   });
 
   describe('verifyInputFunctionNameIsNullOrString', function() {
-    it('should verify that a task has an inputFunctionName that is null or a string',
+    it('should verify that a task has a null or string inputFunctionName',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyInputFunctionNameIsNullOrString,
+          Tsvs.verifyInputFunctionNameIsNullOrString,
           [' does not have null or a string for ',
             'inputFunctionName for the task at index '].join(''));
       });
@@ -117,10 +122,11 @@ describe('TaskSchemaValidationService', function() {
             var auxiliaryCode = item.getAuxiliaryCode('python');
             var tasks = item.getTasks();
             for (var i = 0; i < tasks.length; i++) {
-              // Can't use assertTrueForAllTasks here; need to pass AuxiliaryCode.
-              assertTrue(TaskSchemaValidationService.verifyInputFunctionNameAppearsInAuxiliaryCodeIfNotNull(
-                tasks[i], auxiliaryCode), [
-                  item.getTitle(), message, i].join(''));
+              // Can't use assertTrueForAllTasks here; need AuxiliaryCode.
+              expect(
+                Tsvs.verifyInputFunctionNameAppearsInAuxiliaryCodeIfNotNull(
+                  tasks[i], auxiliaryCode)).toBe(true,
+                    [item.getTitle(), message, i].join(''));
             }
           });
         });
@@ -130,7 +136,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that a task has a null or string outputFunctionName',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyOutputFunctionNameIsNullOrString,
+          Tsvs.verifyOutputFunctionNameIsNullOrString,
           [' does not have null or a string for ',
             'outputFunctionName for the task at index '].join(''));
       });
@@ -148,9 +154,10 @@ describe('TaskSchemaValidationService', function() {
             var tasks = item.getTasks();
             for (var i = 0; i < tasks.length; i++) {
               // Can't use assertTrueForAllTasks here either; auxiliaryCode.
-              assertTrue(TaskSchemaValidationService.verifyOutputFunctionNameAppearsInAuxiliaryCodeIfNotNull(
-                tasks[i], auxiliaryCode), [
-                  item.getTitle(), message, i].join(''));
+              expect(
+                Tsvs.verifyOutputFunctionNameAppearsInAuxiliaryCodeIfNotNull(
+                  tasks[i], auxiliaryCode)).toBe(true,
+                    [item.getTitle(), message, i].join(''));
             }
           });
         });
@@ -160,7 +167,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that prerequisiteSkills are in an array',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyPrerequisiteSkillsAreArray,
+          Tsvs.verifyPrerequisiteSkillsAreArray,
           ' does not have a prerequisiteSkills array for the task at index ');
       });
   });
@@ -169,7 +176,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that acquiredSkills are in an array',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyAcquiredSkillsAreArray,
+          Tsvs.verifyAcquiredSkillsAreArray,
           ' does not have an acquiredSkills array for the task at index ');
       });
   });
@@ -178,7 +185,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that there are no duplicates in any task\'s skills',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyNoDuplicateSkillsInPrompts,
+          Tsvs.verifyNoDuplicateSkillsInPrompts,
           ' should have no duplicate skills in the task at index ');
       });
   });
@@ -187,7 +194,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that each task has at least one acquiredSkill',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyAtLeastOneAcquiredSkill,
+          Tsvs.verifyAtLeastOneAcquiredSkill,
           ' does not have at least one acquiredSkill on the task at index ');
       });
   });
@@ -196,7 +203,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that the correctnessTests are stored in an array',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyCorrectnessTestsAreArray,
+          Tsvs.verifyCorrectnessTestsAreArray,
           ' does not have an array of correctnessTests in the task at index ');
       });
   });
@@ -205,8 +212,9 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that there is at least one correctness test on each task',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyAtLeastOneCorrectnessTest,
-          ' does not have at least one correctness test for the task at index ');
+          Tsvs.verifyAtLeastOneCorrectnessTest,
+          [' does not have at least one correctness test for ',
+            'the task at index '].join(''));
       });
   });
 
@@ -214,8 +222,9 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that each correctness test has a nonempty allowedOutput.',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyCorrectnessTestsHaveNonEmptyAllowedOutputArrays,
-          ' does not have a nonempty allowedOutput array for the task at index ');
+          Tsvs.verifyCorrectnessTestsHaveNonEmptyAllowedOutputArrays,
+          [' does not have a nonempty allowedOutput array ',
+            'for the task at index '].join(''));
       });
   });
 
@@ -223,8 +232,9 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that buggy output tests are stored in an array',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyBuggyOutputTestsAreArray,
-          ' does not have buggy output tests in an array for the task at index');
+          Tsvs.verifyBuggyOutputTestsAreArray,
+          [' does not have buggy output tests in an array ',
+            'for the task at index'].join(''));
       });
   });
 
@@ -232,7 +242,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that buggy output test messages are an array of strings',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyNoDuplicateSkillsInPrompts,
+          Tsvs.verifyNoDuplicateSkillsInPrompts,
           ' does not have an array of string messages for the task at index ');
       });
   });
@@ -241,8 +251,9 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that performance tests are stored in an array',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyPerformanceTestsAreArray,
-          ' does not have an array for performance tests for the task at index ');
+          Tsvs.verifyPerformanceTestsAreArray,
+          [' does not have an array for performance tests ',
+            'for the task at index '].join(''));
       });
   });
 
@@ -250,9 +261,9 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that, performance tests have an inputDataAtom string',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyPerformanceTestsHaveInputDataAtomString,
-          [' does not have an inputDataAtom string for the defined performance ',
-            'test in the task at index '].join(''));
+          Tsvs.verifyPerformanceTestsHaveInputDataAtomString,
+          [' does not have an inputDataAtom string for the defined ',
+            'performance test in the task at index '].join(''));
       });
   });
 
@@ -261,7 +272,7 @@ describe('TaskSchemaValidationService', function() {
     it('should verify that existing performance tests have a linear runtime',
       function() {
         assertTrueOnAllTasks(
-          TaskSchemaValidationService.verifyPerformanceTestsHaveLinearExpectedPerformance,
+          Tsvs.verifyPerformanceTestsHaveLinearExpectedPerformance,
           [' does not have a linear expected performance for the defined ',
             'performance test in the task at index '].join(''));
       });
@@ -280,9 +291,9 @@ describe('TaskSchemaValidationService', function() {
           var tasks = item.getTasks();
           for (var i = 0; i < tasks.length; i++) {
             // No assertTrueForAllTasks here either; we need to pass in code.
-            assertTrue(TaskSchemaValidationService.verifyPerformanceTestsHaveEvaluationFunctionName(
-              tasks[i], auxiliaryCode, starterCode), [
-                item.getTitle(), message, i].join(''));
+            expect(Tsvs.verifyPerformanceTestsHaveEvaluationFunctionName(
+              tasks[i], auxiliaryCode, starterCode)).toBe(true,
+                [item.getTitle(), message, i].join(''));
           }
         });
       });
