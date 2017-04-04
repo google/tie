dee952c5d2bbad40742de5edec3a5deaf9d5958f
Fei Luo <feiluo@google.com>
2017-04-03 23:20:49 -0700

    Make feedback page show the message starting from the beginning. fix #83

--- client/components/LearnerViewDirective.js
+++ client/components/LearnerViewDirective.js
@@ -400,6 +400,7 @@ tie.directive('learnerView', [function() {
 
         var setFeedback = function(feedback) {
           $scope.loadingIndicatorIsShown = false;
+          feedbackDiv.scrollTop = 0;
           $scope.feedbackTimestamp = (
             '[' + (new Date()).toLocaleTimeString() + ']');
           if (feedback.isAnswerCorrect()) {
