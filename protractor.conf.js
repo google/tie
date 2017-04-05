3f0ae946040eb77ed88e3e3ebe493800060c4c45
Fei Luo <feiluo@google.com>
2017-04-05 16:43:53 -0700

    Add configuration for end-to-end tests.

---
+++ protractor.conf.js
@@ -0,0 +1,18 @@
+// Protractor configuration for TIE.
+
+exports.config = {
+  framework: 'jasmine',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['testing/page-loading-spec.js'],
+  baseUrl: 'file://' + __dirname,
+  onPrepare: function() {
+    // By default, Protractor use data:text/html,<html></html> as resetUrl, but 
+    // location.replace from the data: to the file: protocol is not allowed
+    // (we'll get ‘not allowed local resource’ error), so we replace resetUrl with one
+    // with the file: protocol (this particular one will open system's root folder)
+    browser.resetUrl = 'file://';
+  }
+};
