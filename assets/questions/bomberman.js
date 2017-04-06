5fe662791a0e0433279686a036b5cc7b7313bb99
Jianhao Lyu <jianhaolyu@google.com>
2017-04-06 13:38:40 -0700

    fix style issues

--- assets/questions/bomberman.js
+++ assets/questions/bomberman.js
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 /**
- * @fileoverview Question data for Bomber man.
+ * @fileoverview Question data for Bomberman.
  */
 
 globalData.questions['bomberman'] = {  // eslint-disable-line dot-notation
@@ -30,38 +30,40 @@ globalData.questions['bomberman'] = {  // eslint-disable-line dot-notation
 
     @classmethod
     def ignoreWalls(cls, board):
-        if not board or not board[0]: return 0
+        if not board or not board[0]:
+            return 0
         rows = len(board)
         cols = len(board[0])
-        maxEnemyKiled = 0
-        up = [0 for x in range(cols)]
-        left = 0
-        enemyKilled = [[0 for x in range(cols)] for y in range(rows)]
+        maxEnemiesKilled = 0
+        enemiesOnTop = [0 for x in range(cols)]
+        enemiesOnLeft = 0
+        enemiesKilled = [[0 for x in range(cols)] for y in range(rows)]
         for r in range(rows):
-            left = 0
+            enemiesOnLeft = 0
             for c in range(cols):
                 if board[r][c] == "e":
-                    left += 1
-                    up[c] += 1
+                    enemiesOnLeft += 1
+                    enemiesOnTop[c] += 1
                 else:
-                    enemyKilled[r][c] += left + up[c]
-        right = 0
-        down = [0 for x in range(cols)]
+                    enemiesKilled[r][c] += enemiesOnLeft + enemiesOnTop[c]
+        enemiesOnRight = 0
+        enemiesOnBot = [0 for x in range(cols)]
         for r in range(rows - 1, -1, -1):
-            right = 0
+            enemiesOnRight = 0
             for c in range(cols - 1, -1, -1):
                 if board[r][c] == "e":
-                    right += 1
-                    down[c] += 1
+                    enemiesOnRight += 1
+                    enemiesOnBot[c] += 1
                 else:
-                    enemyKilled[r][c] += right + down[c]
-                    if enemyKilled[r][c] > maxEnemyKilled:
-                        maxEnemyKiled = enemyKilled[r][c]
-        return maxEnemyKilled
+                    enemiesKilled[r][c] += enemiesOnRight + enemiesOnBot[c]
+                    if enemiesKilled[r][c] > maxEnemiesKilled:
+                        maxEnemiesKilled = enemiesKilled[r][c]
+        return maxEnemiesKilled
 
     @classmethod
     def bombAllEnemiesOnBoard(cls, board):
-        if not board or not board[0]: return 0
+        if not board or not board[0]:
+            return 0
         count = 0
         for r in range(len(board)):
             for c in range(len(board[0])):
@@ -74,15 +76,15 @@ globalData.questions['bomberman'] = {  // eslint-disable-line dot-notation
     instructions: [
       [
         'For this question, you will be given a board, in which all elements are strings. ',
-        'x represents a wall, e represents an enemy, "" represents an empty cell. ',
+        '"x" represents a wall, "e" represents an enemy, "" represents an empty cell. ',
         'You can place an bomb in an empty cell, which will kill all enemies in vertical and horizontal ',
-        'direction until a wall or board boundary is met. ',
-        'Please write a function bomb(board), to find the best position to place a bomb in order to',
+        'direction until the blast hits a wall or the edge of the board.',
+        'Write a function bomb(board), to find the best position to place a bomb in order to',
         'kill as many enemies as possible, then return the number of enemies killed.'
       ].join('')
     ],
-    prerequisiteSkills: ['Arrays', 'Strings', 'String Manipulation'],
-    acquiredSkills: ['String Manipulation'],
+    prerequisiteSkills: ['Arrays', 'Strings'],
+    acquiredSkills: ['Dynamic Programming'],
     inputFunctionName: null,
     outputFunctionName: null,
     mainFunctionName: 'bomb',
@@ -118,8 +120,8 @@ globalData.questions['bomberman'] = {  // eslint-disable-line dot-notation
     ],
     buggyOutputTests: [{
       buggyFunctionName: 'AuxiliaryCode.ignoreWalls',
-      messages: [["Try to run your code on ['e', '', 'x', 'e']. ",
-        "Is the result match what you expected? "
+      messages: [["Try to run your code on [['e', '', 'x', 'e']]. ",
+        "Does the result match what you expected? "
       ].join(''),
         [
           "You seem to be ignoring walls in your solution, which is incorrect.",
@@ -129,8 +131,8 @@ globalData.questions['bomberman'] = {  // eslint-disable-line dot-notation
     },
     {
       buggyFunctionName: 'AuxiliaryCode.bombAllEnemiesOnBoard',
-      messages: [["Try to run your code on ['e', 'x', '', 'x', 'e']. ",
-        "Is the result match what you expected?"
+      messages: [["Try to run your code on [['e', 'x', '', 'x', 'e']]. ",
+        "Does the result match what you expected?"
       ].join(''),
         [
           "You seemed to be returning all the enemies on the board. ",
