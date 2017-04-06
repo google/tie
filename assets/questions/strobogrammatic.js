3bea4cf83389cbf64ae40a3a1219348d4ae8ec03
Yining Gong <yinigong@yinigong.mtv.corp.google.com>
2017-04-06 12:39:12 -0700

    fix problems according to feedback

--- assets/questions/strobogrammatic.js
+++ assets/questions/strobogrammatic.js
@@ -28,20 +28,21 @@ globalData.questions['strobogrammatic'] = {  // eslint-disable-line dot-notation
     python:
 `class AuxiliaryCode(object):
     @classmethod
-    def firstDigitZero(cls, length):
-        result = [["00","11","69","88","96"]]
-        return result
+    def firstDigitShouldNotBeZero(cls, length):
+        if length == 2:
+           return [["00","11","69","88","96"]]
 
     @classmethod
-    def forgetZero(cls, word):
-        return [["1","8"]]
-
+    def forgetZero(cls, length):
+        if length == 1:
+           return [["1","8"]]
 `
   },
   tasks: [{
     instructions: [[ 
-      'Implement a function which takes a number N as input and generate a list of ',
+      'Implement a function which takes a number N as input and generates a list of ',
       'numbers which has length N and has rotational symmetry ',
+      'rotational symmetry means you will get same number when the number is rotated 180 degree',
       'For example, for length 2 , we have [11, 69, 88, 96] '
     ].join('')
     ],
@@ -61,15 +62,24 @@ globalData.questions['strobogrammatic'] = {  // eslint-disable-line dot-notation
       allowedOutputs: [['101', '111', '181', '609', '619', '689', '808', '818', '906', '916', '986']]
     }],
     buggyOutputTests: [{
-      buggyFunctionName: 'AuxiliaryCode.firstDigitZero',
+      buggyFunctionName: 'AuxiliaryCode.forgetZero',
       messages: [
-        "You seem forget 0 is also number with rotational symmetry."
+        "Try running your code on length 1. Did you expect that result?",
+        [
+          'Are you making sure you get all the answers?',
+          '0 should also be included'
+        ].join('')
       ]
     },
     {
-      buggyFunctionName: 'AuxiliaryCode.firstDigitZero',
+      buggyFunctionName: 'AuxiliaryCode.firstDigitShouldNotBeZero',
       messages: [
-        "0 is not allowed as the first digit unless it's the only digit"
+        "Try running your code on length 2. Did you expect that result?",
+        [
+          'Are you making sure you don\'t include any strange format number?',
+          'like 08?'
+        ].join(''),
+        "You should not include 00 in your answer."
       ]
     }],
     performanceTests: []
