9c58ecdb87584fb6a9badd44c1ff799c84bedd3a
Yilun Chong <yilunchong@google.com>
2017-04-10 09:58:03 -0700

    add question unknownAlphabet

---
+++ assets/questions/unknownAlphabet.js
@@ -0,0 +1,324 @@
+// Copyright 2017 The TIE Author, s. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Question data for Unknown Alphabet.
+ */
+
+globalData.questions['unknownAlphabet'] = {  // eslint-disable-line dot-notation
+  title: 'Unknow Alphabet',
+  starterCode: {
+    python:
+`def findDictionary(words):
+    return ""`
+  },
+  auxiliaryCode: {
+    python:
+`
+class AuxiliaryCode(object):
+  @classmethod
+  def failsOnNoSolution(cls, words):
+    """
+      edges stores the edges that need to build for the alphabet graph
+      edges[ch] is a list of letters for letter ch whose ranks are bigger than
+      ch's rank.
+      degree stores the degree of each letter.
+      For each letter ch, there're degree[ch] letters has an edge goes to ch
+    """
+    edges = {}
+    degree = {}
+    for word in words:
+      for ch in word:
+        ch = ch.lower()
+        if(not edges.has_key(ch)):
+          edges[ch] = []
+          degree[ch] = 0
+    for wordIndex in range(len(words) - 1):
+      for letterIndex in range(min(len(words[wordIndex]), len(words[wordIndex + 1]))):
+        letterOne = words[wordIndex][letterIndex].lower()
+        letterTwo = words[wordIndex + 1][letterIndex].lower()
+        if(letterOne != letterTwo):
+          edges[letterOne].append(letterTwo)
+          degree[letterTwo] = degree[letterTwo] + 1
+          break
+    answer  = ""
+    hasZeroDegreeLetter = True
+    while hasZeroDegreeLetter :
+      hasZeroDegreeLetter = False
+      for key in degree:
+        if degree[key] == 0:
+          hasZeroDegreeLetter = True
+          answer = answer + key
+          for nextLetter in edges[key]:
+            degree[nextLetter] = degree[nextLetter] - 1
+          del degree[key]
+      if not hasZeroDegreeLetter:
+        break
+    return answer
+    
+    
+  @classmethod
+  def failsO nUppercase(cls, words):
+    edges = {}
+    degree = {}
+    for word in words:
+      for ch in word:
+        if(not edges.has_key(ch)):
+          edges[ch] = []
+          degree[ch] = 0
+    for wordIndex in range(len(words) - 1):
+      for letterIndex in range(min(len(words[wordIndex]), len(words[wordIndex + 1]))):
+        letterOne = words[wordIndex][letterIndex]
+        letterTwo = words[wordIndex + 1][letterIndex]
+        if letterOne != letterTwo:
+          edges[letterOne].append(letterTwo)
+          degree[letterTwo] = degree[letterTwo] + 1
+          break
+    answer  = ""
+    hasZeroDegreeLetter = True
+    while hasZeroDegreeLetter :
+      hasZeroDegreeLetter = False
+      for key in degree:
+        if degree[key] == 0:
+          hasZeroDegreeLetter = True
+          answer = answer + key
+          for nextLetter in edges[key]:
+            degree[nextLetter] = degree[nextLetter] - 1
+          del degree[key]
+      if not hasZeroDegreeLetter:
+        break
+    if degree:
+      return ""
+    return answer
+`
+ 
+  },
+  tasks: [{
+    instructions: [
+      [
+        'Given a dictionary (a list of words in lexicographic order) of all',
+        'words in an unknown or invented language, write a function findDictonary ',
+        'that returns the alphabet (an ordered list of characters) of that language.'
+      ].join(''),
+      'Example dictionary:',
+      '[art, rat, cat, car]',
+      'Alphabet is: "atrc"'
+    ],
+    prerequisiteSkills: ['Topological Sorting', 'String Manipulation'],
+    acquiredSkills: ['String Manipulation'],
+    inputFunctionName: null,
+    outputFunctionName: null,
+    mainFunctionName: 'findDictionary',
+    correctnessTests: [{
+      input: [
+        "a",
+        "ab",
+        "b"
+      ],
+      allowedOutputs: ['ab']
+    }, {
+      input: [
+        "aaec",
+        "aaed",
+        "aac",
+        "abe",
+        "abc",
+        "bed",
+        "bea",
+        "bcc",
+        "bcd"
+      ],
+      allowedOutputs: ['ecdab']
+    }, {
+      input: [
+        "egdi",
+        "egdb",
+        "egej",
+        "egei",
+        "ebjj",
+        "ebjg",
+        "ebgh",
+        "ebgj",
+        "igac",
+        "igae",
+        "igia",
+        "igif",
+        "idcf",
+        "idcc",
+        "idh"
+      ],
+      allowedOutputs: ['afchjgdeib']
+    },
+    {
+      input: [
+        "curwfe",
+        "curwfe",
+        "curwst",
+        "curwsw",
+        "curuyc",
+        "curuyf",
+        "curuga",
+        "curugs",
+        "cuxdeh",
+        "cuxdeu",
+        "cuxdbv",
+        "cuxdbn",
+        "cuxolc",
+        "cuxolb",
+        "cuxojd",
+        "cuxojs",
+        "cpcnow",
+        "cpcnob",
+        "cpcnqu",
+        "cpcnqp",
+        "cpctkj",
+        "cpctkd",
+        "cpctfg",
+        "cpctfa",
+        "cpbjeq",
+        "cpbjec",
+        "cpbjz",
+        "cpbhx",
+        "cpbhc",
+        "khjzs",
+        "khjzu",
+        "khjis",
+        "khjiv",
+        "khfax",
+        "khfay",
+        "khftg",
+        "khftq",
+        "kmima",
+        "kmimd",
+        "kmiej",
+        "kmies",
+        "kmrbm",
+        "kmrbr",
+        "kmrus",
+        "kmrut"
+      ],
+      allowedOutputs: ['ljhmezirxygadoqckfsvntwbup']
+    }],
+    buggyOutputTests: [],
+    performanceTests: []
+  }, {
+    instructions: [
+      [
+        'Next, double-check your code to make sure it handles unexpected cases',
+        ', such as a test case with no solution or a test case with more than ',
+        'one correct answer.'
+      ].join(''),
+      [
+        'For the test case that has no solution, just return "". For the test ',
+        'case that has multiple answers, return the first answer in English ',
+        'lexicographic (alphabetical) order.'
+      ].join(''),
+      'Example dictionary:',
+      '[a, b, a]',
+      'Alphabet is: ""',
+      '[cba]',
+      'Alphabet is: "abc"'
+    ],
+    prerequisiteSkills: ['Topological Sorting', 'String Manipulation'],
+    acquiredSkills: ['String Manipulation'],
+    inputFunctionName: null,
+    outputFunctionName: null,
+    mainFunctionName: 'findDictionary',
+    correctnessTests: [{
+      input: [
+        "a",
+        "b",
+        "a"
+      ],
+      allowedOutputs: ['']
+    }, {
+      input: [
+        "c",
+        "a",
+        "b",
+        "a"
+      ],
+      allowedOutputs: ['']
+    }, {
+      input: [
+        "cab",
+        "bc"
+      ],
+      allowedOutputs: ['acb']
+    }],
+    buggyOutputTests: [{
+      buggyFunctionName: 'AuxiliaryCode.wrongInNoSolution',
+      messages: [
+        [
+          'What if the test case is ["c", "a", "b", "a"]?',
+          'And what answer your code will generate for this test case?'
+        ].join(''),
+        [
+          'The answer to that case should be a empty string. But what your code',
+          ' have returned?'
+        ].join('')
+      ]
+    }],
+    performanceTests: []
+  }, {
+    instructions: [
+      [
+        'Finally, modify your code to handle strings with a mix of lowercase ',
+        'and uppercase letters. Assume that a lowercase and uppercase version ',
+        'of a letter is the same with regards to lexicographic ordering. Your ',
+        'answer, though, should be lowercase.'
+      ].join(''),
+      'Example dictionary:',
+      '[b, Ba]',
+      'Alphabet is: "ab"'
+    ],
+    prerequisiteSkills: ['Topological Sorting', 'String Manipulation'],
+    acquiredSkills: ['String Manipulation'],
+    inputFunctionName: null,
+    outputFunctionName: null,
+    mainFunctionName: 'findDictionary',
+    correctnessTests: [{
+      input: [
+        "ab",
+        "Aba",
+        "b"
+      ],
+      allowedOutputs: ['ab']
+    }],
+    buggyOutputTests: [{
+      buggyFunctionName: 'AuxiliaryCode.wrongInUppercase',
+      messages: [
+        [
+          'Try running your code over ["a", "Ab", "b"]. Do you get the result ',
+          'you expected?'
+        ].join(''),
+        'How does your code handle uppercase letters?',
+        [
+          'The answer to the test case ["a", "Ab", "b"] should be "ab". Did ',
+          'your code return the string with some letters in uppercase?'
+        ].join('')
+      ]
+    }],
+    performanceTests: []
+  }],
+  styleTests: [{
+    evaluationFunctionName: 'allowOnlyOneFunction',
+    expectedOutput: true,
+    message: [
+      'You should only be writing code in an findDictionary function. While ',
+      "decomposition is generally a good idea, you shouldn't need more than ",
+      'just this function for this question.'
+    ].join('')
+  }]
+};
--- assets/tests/QuestionSchemaValidationServiceSpec.js
+++ assets/tests/QuestionSchemaValidationServiceSpec.js
@@ -27,7 +27,7 @@ describe('QuestionSchemaValidationService', function() {
   // Should contain all question IDs.
   // TODO(eyurko): Figure out a way to dynamically check to make sure
   // that all question IDs are specified.
-  var QUESTION_IDS = ['reverseWords', 'parens', 'i18n', 'rle'];
+  var QUESTION_IDS = ['reverseWords', 'parens', 'i18n', 'rle', 'unknownAlphabet'];
 
   beforeEach(module('tie'));
   beforeEach(inject(function($injector) {
