939ef19066b49771f2bdb30c6a9ebdfee1c745d4
Chenwei Cai <caichenwei1991@gmail.com>
2017-04-11 15:09:55 -0700

    removed from string.js

--- assets/question_sets/strings.js
+++ assets/question_sets/strings.js
@@ -26,5 +26,5 @@ globalData.questionSets['strings'] = {  // eslint-disable-line dot-notation
       "I will provide you with feedback."
     ].join('')
   ],
-  questionIds: ['bestMeetupLocation', 'reverseWords', 'parens', 'i18n', 'rle']
+  questionIds: ['reverseWords', 'parens', 'i18n', 'rle']
 };
