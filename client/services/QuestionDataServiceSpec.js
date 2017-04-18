6162032c10f3cf1311b0f76c0e50e0642560d06a
arbiter527 <xzw@google.com>
2017-04-18 14:21:50 -0700

    fix issue 223 add unit test for QuestionDataServiceSpec

---
+++ client/services/QuestionDataServiceSpec.js
@@ -0,0 +1,65 @@
+// Copyright 2017 The TIE Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for the QuestionDataService.
+ */
+ 
+describe('QuestionDataService', function() {
+  var QuestionDataService;
+  var QUESTION_IDS = Object.keys(globalData.questions);
+ 
+  beforeEach(module('tie'));
+  beforeEach(inject(function($injector) {
+    globalData.questionSets.all = {};
+    globalData.questionSets.all.questionIds = QUESTION_IDS;
+    globalData.questionSets.all.introductionParagraphs = [];
+    
+    QuestionDataService = $injector.get('QuestionDataService');
+  }));
+ 
+  describe('initCurrentQuestionSet', function() { 
+    it('should throw an error if the question set id does not exist',
+    function() {
+      expect(function() {
+        QuestionDataService.initCurrentQuestionSet('other');
+      }).toThrow();
+    });
+  });
+ 
+  describe('getCurrentQuestionSet', function() {
+    it('should correctly get current question set', function() {
+      QuestionDataService.initCurrentQuestionSet('all');
+      var questionSet = QuestionDataService.getCurrentQuestionSet('all');
+
+      expect(questionSet.getQuestionIds()).toEqual(QUESTION_IDS);
+    });
+
+    it('should throw an error if the question set id does not exist',
+    function() {
+      expect(function() {
+        QuestionDataService.getCurrentQuestionSet('other');
+      }).toThrow();
+    });
+  });
+ 
+  describe('getQuestion', function() {
+    it('should throw an error if the question id does not exist', function() {
+      QuestionDataService.initCurrentQuestionSet('all');
+      expect(function() {
+        QuestionDataService.getQuestion('');
+      }).toThrow();
+    });
+  });
+});
