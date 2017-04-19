befbe81c0349dfe23f778b48fa09939ea9489b8a
wmcayj <wmcalyj@hotmail.com>
2017-04-19 11:53:24 -0700

    Address review comments

--- client/components/LearnerViewDirective.js
+++ client/components/LearnerViewDirective.js
@@ -80,7 +80,7 @@ tie.directive('learnerView', [function() {
                     Reset Code
                   </button>
                   <div class="tie-code-auto-save" ng-show="autosaveTextIsDisplayed">
-                    Saving code now...
+                    Saving code...
                   </div>
                   <button class="tie-run-button"
                       ng-class="{'active': !nextButtonIsShown}"
@@ -397,11 +397,13 @@ tie.directive('learnerView', [function() {
     controller: [
       '$scope', '$interval', '$timeout', 'SolutionHandlerService',
       'QuestionDataService', 'LANGUAGE_PYTHON', 'FeedbackObjectFactory',
-      'CodeStorageService',
+      'CodeStorageService', 'SECONDS_TO_MILLISECONDS', 'DEFAULT_AUTOSAVE_SECONDS',
+      'DISPLAY_AUTOSAVE_TEXT_SECONDS',
       function(
           $scope, $interval, $timeout, SolutionHandlerService,
           QuestionDataService, LANGUAGE_PYTHON, FeedbackObjectFactory,
-          CodeStorageService) {
+          CodeStorageService, SECONDS_TO_MILLISECONDS, DEFAULT_AUTOSAVE_SECONDS,
+          DISPLAY_AUTOSAVE_TEXT_SECONDS) {
         var DURATION_MSEC_WAIT_FOR_SCROLL = 20;
         var language = LANGUAGE_PYTHON;
         // TODO(sll): Generalize this to dynamically select a question set
@@ -419,14 +421,8 @@ tie.directive('learnerView', [function() {
           {themeName: 'Dark'}
         ];
 
-        var SECONDS_TO_MILLISECONDS = 1000;
-        // Default time interval, in seconds, after which code will
-        // be auto-saved.
-        var DEFAULT_AUTOSAVE_SECONDS = 5;
-        // "Saving code..." will last for 1 seconds and disappear.
-        var DISPLAY_AUTOSAVE_TEXT_SECONDS = 1;
-
         var autosaveCancelPromise;
+        var localCodeCache;
         var congratulatoryFeedback = FeedbackObjectFactory.create();
         QuestionDataService.initCurrentQuestionSet(questionSetId);
         var questionSet = QuestionDataService.getCurrentQuestionSet(
@@ -449,11 +445,11 @@ tie.directive('learnerView', [function() {
           question = QuestionDataService.getQuestion(questionId);
           tasks = question.getTasks();
           currentTaskIndex = 0;
-          var storedCode =
+          localCodeCache =
             CodeStorageService.loadStoredCode(questionId, language);
           $scope.title = question.getTitle();
-          $scope.code = storedCode ?
-              storedCode : question.getStarterCode(language);
+          $scope.code = localCodeCache ?
+              localCodeCache : question.getStarterCode(language);
           $scope.instructions = tasks[currentTaskIndex].getInstructions();
           $scope.previousInstructions = [];
           $scope.nextButtonIsShown = false;
@@ -560,7 +556,6 @@ tie.directive('learnerView', [function() {
           // their own code back if they click on the current question.
           CodeStorageService.storeCode(currentQuestionId,
             $scope.code, language);
-
           loadQuestion(questionId, questionSet.getIntroductionParagraphs());
         };
 
@@ -577,7 +572,7 @@ tie.directive('learnerView', [function() {
                 ).then(setFeedback);
             }, DURATION_MSEC_WAIT_FOR_SCROLL);
           }, 0);
-          CodeStorageService.storeCode(
+          storeCodeAndUpdateLocalCodeCache(
             $scope.questionIds[$scope.currentQuestionIndex], code, language);
         };
 
@@ -600,15 +595,13 @@ tie.directive('learnerView', [function() {
             autosaveCancelPromise = $interval(function() {
               var currentQuestionId =
                 $scope.questionIds[$scope.currentQuestionIndex];
-              var storedCode = CodeStorageService.loadStoredCode(
-                currentQuestionId, language);
-              if (angular.equals(storedCode, $scope.code)) {
+              if (angular.equals(localCodeCache, $scope.code)) {
                 // No code change, stop autosave loop.
                 stopAutosave();
               } else {
-                // Code change detected, notify user,
-                // save code and continue this loop.
-                CodeStorageService.storeCode(
+                // Code change detected, notify user, save code,
+                // update code cache and continue this loop.
+                storeCodeAndUpdateLocalCodeCache(
                   currentQuestionId, $scope.code, language);
                 triggerAutosaveNotification(DISPLAY_AUTOSAVE_TEXT_SECONDS);
               }
@@ -619,6 +612,12 @@ tie.directive('learnerView', [function() {
         var stopAutosave = function() {
           $scope.autosaveOn = false;
           $interval.cancel(autosaveCancelPromise);
+        };
+
+        var storeCodeAndUpdateLocalCodeCache = function(
+          questionId, code, lang) {
+          CodeStorageService.storeCode(questionId, code, lang);
+          localCodeCache = code;
         };
 
         loadQuestion(
--- client/components/LearnerViewDirectiveSpec.js
+++ client/components/LearnerViewDirectiveSpec.js
@@ -38,24 +38,23 @@ describe('LearnerViewDirective', function() {
   var NUM_CHARS_CODE = 100;
   var LANGUAGE = 'python';
 
-  // Please make sure the following three variables are the same as
-  // what we have in LearnerViewDirective
-  var SECONDS_TO_MILLISECONDS = 1000;
-  var DEFAULT_AUTOSAVE_SECONDS = 5;
+  var SECONDS_TO_MILLISECONDS;
+  var DEFAULT_AUTOSAVE_SECONDS;
 
   var DELTA_MILLISECONDS = 100;
 
-  var AUTOSAVE_MILLISECONDS =
-    DEFAULT_AUTOSAVE_SECONDS * SECONDS_TO_MILLISECONDS;
+  var AUTOSAVE_MILLISECONDS;
 
   var AUTOSAVE_REPEAT_RANGE = 20;
 
-  beforeEach(inject(function($compile, $rootScope, _QuestionDataService_) {
+  beforeEach(inject(function($compile, $rootScope, _QuestionDataService_,
+    _SECONDS_TO_MILLISECONDS_, _DEFAULT_AUTOSAVE_SECONDS_) {
     $scope = $rootScope.$new();
 
     // The reason why we have to go through this trouble to get $scope
     // is the controller is anonymous, thus, there is no easy way to do
     // it. Recommend future refactor.
+    // TODO
     element = angular.element('<learner-view></learner-view>');
     element = $compile(element)($scope);
     $scope = element.isolateScope();
@@ -64,11 +63,19 @@ describe('LearnerViewDirective', function() {
 
     QuestionDataService = _QuestionDataService_;
 
+    SECONDS_TO_MILLISECONDS = _SECONDS_TO_MILLISECONDS_;
+    DEFAULT_AUTOSAVE_SECONDS = _DEFAULT_AUTOSAVE_SECONDS_;
+    AUTOSAVE_MILLISECONDS =
+      DEFAULT_AUTOSAVE_SECONDS * SECONDS_TO_MILLISECONDS;
+
+    localStorage.clear();
+
+
   }));
 
   describe("resetCode", function() {
 
-    it('should reset code to default code', function() {
+    it('should reset code to starter code', function() {
       $scope.questionIds.forEach(function(questionId, index) {
         var question = QuestionDataService.getQuestion(questionId);
         var starterCode = question.getStarterCode(LANGUAGE);
@@ -78,15 +85,6 @@ describe('LearnerViewDirective', function() {
         $scope.resetCode();
         expect($scope.code).toEqual(starterCode);
       });
-    });
-  });
-
-  describe("navigateToQuestion", function() {
-    it('should navigate to given question', function() {
-      for (var i = 0; i < $scope.questionIds.length; i++) {
-        $scope.navigateToQuestion(i);
-        expect($scope.currentQuestionIndex).toBe(i);
-      }
     });
   });
 
@@ -103,35 +101,35 @@ describe('LearnerViewDirective', function() {
 
     // Autosave text is using $timeout, thus,
     // we need to flush both $interval and $timeout.
-    var timeFlies = function(timeToFly) {
-      $interval.flush(timeToFly);
-      $timeout.flush(timeToFly);
+    var flushIntervalAndTimeout = function(timeToFlush) {
+      $interval.flush(timeToFlush);
+      $timeout.flush(timeToFlush);
     };
 
-    it("should activate autosaving for only once", function() {
+    it("should only activate autosave once", function() {
       for (var i = 0; i < $scope.questionIds.length; i++) {
         if (i !== 0) {
           // Default question index is 0, we don't want to
           // navigate to question 0 again because navigateToQuestion
-          // will store code for current question, thus, fail the test
+          // will already have stored code for that question and fail the test.
           $scope.navigateToQuestion(i);
         }
         expect(!$scope.autosaveOn).toBe(true);
         var questionId = $scope.questionIds[i];
-        // There should be no code stored in local storage
-        // before autosave is triggerred.
+        // There should be no code stored in localStorage
+        // before autosave is triggered.
         expect(CodeStorageService.loadStoredCode(
           questionId, LANGUAGE)).toEqual(null);
         $scope.activateAutosaving();
         expect($scope.autosaveOn).toBe(true);
         // Flush 4900 milliseconds -- time: 4.9s
         expect($scope.autosaveTextIsDisplayed).toBe(false);
-        timeFlies(AUTOSAVE_MILLISECONDS - DELTA_MILLISECONDS);
+        flushIntervalAndTimeout(AUTOSAVE_MILLISECONDS - DELTA_MILLISECONDS);
         expect($scope.autosaveOn).toBe(true);
         expect(CodeStorageService.loadStoredCode(
           questionId, LANGUAGE)).toEqual(null);
         // Flush 200 milliseconds -- time: 5.1s
-        timeFlies(2 * DELTA_MILLISECONDS);
+        flushIntervalAndTimeout(2 * DELTA_MILLISECONDS);
         var question = QuestionDataService.getQuestion(questionId);
         var starterCode = question.getStarterCode(LANGUAGE);
         expect(CodeStorageService.loadStoredCode(
@@ -139,10 +137,10 @@ describe('LearnerViewDirective', function() {
         expect($scope.autosaveOn).toBe(true);
         expect($scope.autosaveTextIsDisplayed).toBe(true);
         // Flush 1000 milliseconds -- time: 6.1s
-        timeFlies(SECONDS_TO_MILLISECONDS);
+        flushIntervalAndTimeout(SECONDS_TO_MILLISECONDS);
         expect($scope.autosaveTextIsDisplayed).toBe(false);
         // Flush 4000 milliseconds -- time: 10.1s
-        timeFlies(4 * SECONDS_TO_MILLISECONDS);
+        flushIntervalAndTimeout(4 * SECONDS_TO_MILLISECONDS);
         expect($scope.autosaveTextIsDisplayed).toBe(false);
         expect($scope.autosaveOn).toBe(false);
         expect(CodeStorageService.loadStoredCode(
@@ -167,34 +165,28 @@ describe('LearnerViewDirective', function() {
         expect(CodeStorageService.loadStoredCode(
           questionId, LANGUAGE)).toEqual(null);
         $scope.activateAutosaving();
-        expect($scope.autosaveOn).toBe(true);
-        // Flush 4900 milliseconds -- time: 4.9s
-        expect($scope.autosaveTextIsDisplayed).toBe(false);
-        timeFlies(AUTOSAVE_MILLISECONDS - DELTA_MILLISECONDS);
-        expect($scope.autosaveOn).toBe(true);
-        expect(CodeStorageService.loadStoredCode(
-          questionId, LANGUAGE)).toEqual(null);
-        // Flush 200 milliseconds -- time: 5.1s
-        timeFlies(2 * DELTA_MILLISECONDS);
-        var question = QuestionDataService.getQuestion(questionId);
-        var starterCode = question.getStarterCode(LANGUAGE);
-        expect(CodeStorageService.loadStoredCode(
-          questionId, LANGUAGE)).toEqual(starterCode);
-        expect($scope.autosaveOn).toBe(true);
-        expect($scope.autosaveTextIsDisplayed).toBe(true);
         var randomCodes;
         for (var j = 0; j < repeatTimes; j++) {
           randomCodes = generateRandomChars(NUM_CHARS_CODE);
           $scope.code = randomCodes;
-          timeFlies(AUTOSAVE_MILLISECONDS);
+          flushIntervalAndTimeout(AUTOSAVE_MILLISECONDS);
         }
         expect(CodeStorageService.loadStoredCode(
           questionId, LANGUAGE)).toEqual(randomCodes);
-        timeFlies(AUTOSAVE_MILLISECONDS);
+        flushIntervalAndTimeout(AUTOSAVE_MILLISECONDS);
         expect($scope.autosaveTextIsDisplayed).toBe(false);
         expect($scope.autosaveOn).toBe(false);
         expect(CodeStorageService.loadStoredCode(
           questionId, LANGUAGE)).toEqual(randomCodes);
+      }
+    });
+  });
+
+  describe("navigateToQuestion", function() {
+    it('should navigate to given question', function() {
+      for (var i = 0; i < $scope.questionIds.length; i++) {
+        $scope.navigateToQuestion(i);
+        expect($scope.currentQuestionIndex).toBe(i);
       }
     });
   });
