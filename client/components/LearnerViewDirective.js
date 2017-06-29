f272de3bb28ceb8cc561c76902aa8e8a0f303f43
dchen97 <dianakc@google.com>
2017-06-29 16:03:57 -0700

    Get rid of dummy code... again

--- client/components/LearnerViewDirective.js
+++ client/components/LearnerViewDirective.js
@@ -142,7 +142,6 @@ tie.directive('learnerView', [function() {
             </div>
           </div>
         </div>
-        <a href="#" ng-click="showExampleModal()">Show Example Modal</a>
       </div>
       <modal></modal>
       <style>
