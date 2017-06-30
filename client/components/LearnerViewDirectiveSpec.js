eee0e53c4f1cf0e406737439abd2524bf3d2b11e
dchen97 <dianakc@google.com>
2017-06-30 08:51:51 -0700

    Add tests for onPrivacyClick

--- client/components/LearnerViewDirectiveSpec.js
+++ client/components/LearnerViewDirectiveSpec.js
@@ -218,6 +218,29 @@ describe('LearnerViewDirective', function() {
     });
   });
 
+  describe("onPrivacyClick", function() {
+    it(['should change the modalTitle and modalDescription correctly if in',
+      'browser-only form'].join(" "), function() {
+        expect($scope.modalTitle).toEqual("");
+        expect($scope.modalDescription).toEqual("");
+
+        $scope.onPrivacyClick();
+
+        expect($scope.modalTitle).toEqual("Privacy Notice:");
+        expect($scope.modalDescription).toEqual(
+          ["This version of the TIE application stores information, ",
+            "including your code, in your browser's local storage and ",
+            "does not transmit data to any server."].join(""));
+      }
+    );
+
+    it('should display the modal', function() {
+      expect($scope.modalIsDisplayed).toBe(false);
+      $scope.onPrivacyClick();
+      expect($scope.modalIsDisplayed).toBe(true);
+    });
+  });
+
   describe("navigateToQuestion", function() {
     it('should navigate to given question', function() {
       for (var i = 0; i < $scope.questionIds.length; i++) {
