09c1d7e87093c7a6eb001909e0735b96d92e9e72
eledavi@google.com <eledavi@google.com>
2017-07-13 14:23:12 -0700

    Updated TaskObjectFactorySpec.js file

---
+++ client/domain/TaskObjectFactorySpec.js
@@ -0,0 +1,65 @@
+// Copyright 2017 The TIE Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Unit tests for TaskObject domain objects.
+ */
+
+describe('TaskObjectFactory', function() {
+  var QuestionObjectFactory;
+  var question;
+
+  var TITLE = 'title';
+  var STARTER_CODE = 'starterCode';
+  var AUXILIARY_CODE = 'auxiliaryCode';
+  var OUTPUT_FUNCTION_NAME_1 =
+  'AuxiliaryCode_AuxiliaryCodeOutputFunctionNameWithoutClass';
+  var OUTPUT_FUNCTION_NAME_2 = 'System_SystemOutputFunctionNameWithoutClass';
+
+  beforeEach(module('tie'));
+  beforeEach(inject(function($injector) {
+    QuestionObjectFactory = $injector.get("QuestionObjectFactory");
+
+    question = QuestionObjectFactory.create({
+      title: TITLE,
+      starterCode: STARTER_CODE,
+      auxiliaryCode: AUXILIARY_CODE,
+      tasks: [{
+        outputFunctionName: OUTPUT_FUNCTION_NAME_1,
+        correctnessTests: [],
+        buggyOutputTests: [],
+        performanceTests: []
+      },
+      {
+        outputFunctionName: OUTPUT_FUNCTION_NAME_2,
+        correctnessTests: [],
+        buggyOutputTests: [],
+        performanceTests: []
+      }]
+    });
+
+  }));
+
+  describe('getOutputFunctionNameWithoutClass', function() {
+    it('should properly get OutputFunctionName without the class name',
+      function() {
+        expect(question.getTasks()[0].getOutputFunctionNameWithoutClass())
+        .toEqual(
+          'AuxiliaryCodeOutputFunctionNameWithoutClass');
+        expect(question.getTasks()[1].getOutputFunctionNameWithoutClass())
+        .toEqual(
+          'SystemOutputFunctionNameWithoutClass');
+      });
+  });
+});
