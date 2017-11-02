google/tie

commit 200c144fc230436b357d81b49b057f0ca757c09a

Add functionality for suite-level tests (#425)
* Simplify feedback generator service to handle both buggy-output and suite-level tests gracefully.

* Fix Karma tests.

* Force created Feedback objects to have an error category.

* Standardize naming to 'feedbackCategory'.

* Fix print feedback.

* Add Karma tests.

* Fix lint issues

* Address review comment.

* Integrate stuff from #424.

* Address review comments.

client/question/services/SolutionHandlerServiceSpec.js
@@ -369,8 +369,7 @@ describe('SolutionHandlerService', function() {
 
     describe('buggy output ignored test suites', function() {
       beforeEach(inject(function() {
-        // Reconfigure the test suites and buggy output tests for the first
-        // task.
+        // Reconfigure the test suites for the first task.
         taskDict[0].testSuites = [{
           id: 'SUITE1',
           humanReadableName: 'suite 1',
@@ -442,5 +441,111 @@ describe('SolutionHandlerService', function() {
         });
       });
     });
+
+    describe('suite-level tests', function() {
+      beforeEach(inject(function() {
+        // Reconfigure the test suites, buggy output tests and suite-level
+        // tests for the first task.
+        taskDict[0].testSuites = [{
+          id: 'SUITE1',
+          humanReadableName: 'suite 1',
+          testCases: [{
+            input: 'task_1_suite_1_test_1',
+            allowedOutputs: [true]
+          }, {
+            input: 'task_1_suite_1_test_2',
+            allowedOutputs: [true]
+          }]
+        }, {
+          id: 'SUITE2',
+          humanReadableName: 'suite 2',
+          testCases: [{
+            input: 'task_1_suite_2_test_1',
+            allowedOutputs: [false]
+          }, {
+            input: 'task_1_suite_2_test_2',
+            allowedOutputs: [false]
+          }]
+        }];
+        taskDict[0].buggyOutputTests = [];
+        taskDict[0].suiteLevelTests = [{
+          testSuiteIdsThatMustPass: ['SUITE1'],
+          testSuiteIdsThatMustFail: ['SUITE2'],
+          messages: ['suite_message1']
+        }];
+      }));
+
+      it('should return suite-level feedback if the condition is triggered',
+        function(done) {
+          orderedTasks = taskDict.map(function(task) {
+            return TaskObjectFactory.create(task);
+          });
+
+          // This code passes suite 1 and fails suite 2.
+          var studentCode = [
+            'def mockMainFunction(input):',
+            '    return True',
+            ''
+          ].join('\n');
+
+          SolutionHandlerService.processSolutionAsync(
+            orderedTasks, starterCode, studentCode, auxiliaryCode, 'python'
+          ).then(function(feedback) {
+            expect(feedback.getParagraphs()[0].getContent()).toBe(
+              'suite_message1');
+            done();
+          });
+        }
+      );
+
+      it([
+        'should not return suite-level feedback if the passing-suite ',
+        ' prerequisites do not hold'
+      ].join(''), function(done) {
+        orderedTasks = taskDict.map(function(task) {
+          return TaskObjectFactory.create(task);
+        });
+
+        // This code passes one test case in suite 1 and fails the other, thus
+        // failing the suite.
+        var studentCode = [
+          'def mockMainFunction(input):',
+          '    return input == "task_1_suite_1_test_1"',
+          ''
+        ].join('\n');
+
+        SolutionHandlerService.processSolutionAsync(
+          orderedTasks, starterCode, studentCode, auxiliaryCode, 'python'
+        ).then(function(feedback) {
+          expect(feedback.getParagraphs()[0].getContent()).toBe(
+            'Your code produced the following result:');
+          done();
+        });
+      });
+
+      it([
+        'should consider a suite failed if at least one test in it fails'
+      ].join(''), function(done) {
+        orderedTasks = taskDict.map(function(task) {
+          return TaskObjectFactory.create(task);
+        });
+
+        // This code passes suite 1, and passes one of the two tests in suite 2.
+        var studentCode = [
+          'def mockMainFunction(input):',
+          '    return input != "task_1_suite_2_test_2"',
+          ''
+        ].join('\n');
+
+        SolutionHandlerService.processSolutionAsync(
+          orderedTasks, starterCode, studentCode, auxiliaryCode, 'python'
+        ).then(function(feedback) {
+          expect(feedback.getParagraphs()[0].getContent()).toBe(
+            'suite_message1');
+          done();
+        });
+      });
+    });
+
   });
 });

client/data/services/QuestionDataService.js
@@ -77,13 +77,14 @@ tieData.factory('QuestionDataService', [
       },
       /**
        * Returns the question's version.
-       * Currently returns null, as question versioning isn't implemented yet.
+       * Currently always returns 1, as question versioning isn't implemented
+       * yet.
        * TODO(eyurko): Return correct question version, once implemented.
        *
-       * @returns {null}
+       * @returns {int} Currently the number 1.
        */
       getQuestionVersion: function() {
-        return null;
+        return 1;
       },
       /**
        * Returns the question's shortened instructions, which includes

client/data/services/QuestionDataServiceSpec.js
@@ -83,10 +83,10 @@ describe('QuestionDataService', function() {
   });
 
   describe('getQuestionVersion', function() {
-    it('should return null exclusively, for now', function() {
+    it('should return 1 exclusively, for now', function() {
       QuestionDataService.initCurrentQuestionSet('strings');
       expect(QuestionDataService.getQuestionVersion())
-        .toEqual(null);
+        .toEqual(1);
     });
   });
 

client/question/components/LearnerViewDirective.js
@@ -26,11 +26,6 @@ tie.directive('learnerView', [function() {
           <div class="tie-question-ui-inner">
             <div class="tie-question-ui">
               <div class="tie-question-window" ng-class="{'night-mode': isInDarkMode}">
-                <div class="tie-greetings">
-                  <p ng-repeat="paragraph in greetingParagraphs track by $index">
-                      {{paragraph.getContent()}}
-                  </p>
-                </div>
                 <h3 class="tie-question-title">{{title}}</h3>
                 <div class="tie-previous-instructions">
                   <div ng-repeat="previousInstruction in previousInstructions track by $index">
@@ -505,13 +500,15 @@ tie.directive('learnerView', [function() {
       'EventHandlerService', 'LocalStorageService', 'ServerHandlerService',
       'SessionIdService', 'SECONDS_TO_MILLISECONDS', 'DEFAULT_AUTOSAVE_SECONDS',
       'DISPLAY_AUTOSAVE_TEXT_SECONDS', 'SERVER_URL', 'DEFAULT_QUESTION_ID',
+      'FEEDBACK_CATEGORIES',
       function(
           $scope, $interval, $timeout, $location, CookieStorageService,
           SolutionHandlerService, QuestionDataService, LANGUAGE_PYTHON,
           FeedbackObjectFactory, ReinforcementObjectFactory,
           EventHandlerService, LocalStorageService, ServerHandlerService,
           SessionIdService, SECONDS_TO_MILLISECONDS, DEFAULT_AUTOSAVE_SECONDS,
-          DISPLAY_AUTOSAVE_TEXT_SECONDS, SERVER_URL, DEFAULT_QUESTION_ID) {
+          DISPLAY_AUTOSAVE_TEXT_SECONDS, SERVER_URL, DEFAULT_QUESTION_ID,
+          FEEDBACK_CATEGORIES) {
         /**
          * Number of milliseconds for TIE to wait for system to process code
          * submission.
@@ -627,7 +624,8 @@ tie.directive('learnerView', [function() {
          *
          * @type {Feedback}
          */
-        var congratulatoryFeedback = FeedbackObjectFactory.create();
+        var congratulatoryFeedback = FeedbackObjectFactory.create(
+          FEEDBACK_CATEGORIES.SUCCESSFUL, true);
 
         /**
          * Stores the current question that the user is working on.
@@ -758,7 +756,7 @@ tie.directive('learnerView', [function() {
 
         /**
          * Initializes the appropriate values in $scope for the question
-         * instructions, stored code, starter code, feedback, and greetings.
+         * instructions, stored code, starter code and feedback.
          *
          * @param {string} questionId ID of question whose data will be loaded
          */
@@ -788,8 +786,6 @@ tie.directive('learnerView', [function() {
           $scope.instructions = tasks[currentTaskIndex].getInstructions();
           $scope.previousInstructions = [];
           $scope.nextButtonIsShown = false;
-          var feedback = FeedbackObjectFactory.create();
-          $scope.greetingParagraphs = feedback.getParagraphs();
           EventHandlerService.createQuestionStartEvent(
             SessionIdService.getSessionId(), $scope.currentQuestionId,
             QuestionDataService.getQuestionVersion());
@@ -858,8 +854,8 @@ tie.directive('learnerView', [function() {
         $scope.setFeedback = function(feedback, code) {
           EventHandlerService.createCodeSubmitEvent(
             SessionIdService.getSessionId(),
-            feedback.getParagraphsAsListOfDicts(), feedback.getErrorCategory(),
-            code, feedback.isAnswerCorrect());
+            feedback.getParagraphsAsListOfDicts(),
+            feedback.getFeedbackCategory(), code, feedback.isAnswerCorrect());
           $scope.loadingIndicatorIsShown = false;
           if (feedback.isAnswerCorrect()) {
             if (question.isLastTask(currentTaskIndex)) {

client/question/components/LearnerViewDirectiveSpec.js
@@ -23,6 +23,7 @@ describe('LearnerViewDirective', function() {
   var QuestionDataService;
   var EventHandlerService;
   var FeedbackObjectFactory;
+  var FEEDBACK_CATEGORIES;
   var $location;
 
   beforeEach(module('tie'));
@@ -50,9 +51,10 @@ describe('LearnerViewDirective', function() {
 
   var QUESTION_ID = 'reverseWords';
 
-  beforeEach(inject(function($compile, $rootScope, _QuestionDataService_,
-    _SECONDS_TO_MILLISECONDS_, _DEFAULT_AUTOSAVE_SECONDS_, _$location_,
-    _EventHandlerService_, _FeedbackObjectFactory_) {
+  beforeEach(inject(function(
+      $compile, $rootScope, $injector, _QuestionDataService_,
+      _SECONDS_TO_MILLISECONDS_, _DEFAULT_AUTOSAVE_SECONDS_, _$location_,
+      _EventHandlerService_, _FeedbackObjectFactory_) {
     $scope = $rootScope.$new();
 
     // The reason why we have to go through this trouble to get $scope
@@ -75,6 +77,7 @@ describe('LearnerViewDirective', function() {
     DEFAULT_AUTOSAVE_SECONDS = _DEFAULT_AUTOSAVE_SECONDS_;
     AUTOSAVE_MILLISECONDS =
       DEFAULT_AUTOSAVE_SECONDS * SECONDS_TO_MILLISECONDS;
+    FEEDBACK_CATEGORIES = $injector.get('FEEDBACK_CATEGORIES');
 
     localStorage.clear();
 
@@ -369,7 +372,9 @@ describe('LearnerViewDirective', function() {
         '    return result',
         ''
       ].join('\n');
-      $scope.setFeedback(FeedbackObjectFactory.create(false), code);
+      $scope.setFeedback(
+        FeedbackObjectFactory.create(FEEDBACK_CATEGORIES.RUNTIME_ERROR, false),
+        code);
       expect(EventHandlerService.createCodeSubmitEvent).toHaveBeenCalled();
     });
   });

client/question/domain/FeedbackObjectFactory.js
@@ -19,7 +19,10 @@
 
 tie.factory('FeedbackObjectFactory', [
   'FeedbackParagraphObjectFactory', 'ReinforcementObjectFactory',
-  function(FeedbackParagraphObjectFactory, ReinforcementObjectFactory) {
+  'FEEDBACK_CATEGORIES',
+  function(
+      FeedbackParagraphObjectFactory, ReinforcementObjectFactory,
+      FEEDBACK_CATEGORIES) {
     /**
      * Feedback objects contain encapsulate all of the information
      * - including personalized feedback and Reinforcement - that are used to
@@ -29,11 +32,17 @@ tie.factory('FeedbackObjectFactory', [
     /**
      * Constructor for Feedback
      *
+     * @param {string} feedbackCategory The category of the feedback. Must be
+     *    a valid entry in FEEDBACK_CATEGORIES.
      * @param {boolean} answerIsCorrect indicates whether student's answer
      *    is correct or not
      * @constructor
      */
-    var Feedback = function(answerIsCorrect) {
+    var Feedback = function(feedbackCategory, answerIsCorrect) {
+      if (!FEEDBACK_CATEGORIES.hasOwnProperty(feedbackCategory)) {
+        throw Error('Invalid feedback category: ' + feedbackCategory);
+      }
+
       /**
        * List of FeedbackParagraph objects associated with this feedback.
        *
@@ -58,12 +67,12 @@ tie.factory('FeedbackObjectFactory', [
       this._hintIndex = null;
 
       /**
-       * Records what type of error was found in the submission, or 'NO ERROR'.
+       * Records the category corresponding to this feedback.
        *
        * @type {string}
        * @private
        */
-      this._errorCategory = null;
+      this._feedbackCategory = feedbackCategory;
 
       /**
        * @type {Reinforcement}
@@ -92,6 +101,16 @@ tie.factory('FeedbackObjectFactory', [
      */
     Feedback.prototype.isAnswerCorrect = function() {
       return this._answerIsCorrect;
+    };
+
+    /**
+     * Prepends a FeedbackParagraph of type text to the _paragraphs Array.
+     *
+     * @param {string} text String to be inserted in text paragraph
+     */
+    Feedback.prototype.prependTextParagraph = function(text) {
+      this._paragraphs.unshift(
+        FeedbackParagraphObjectFactory.createTextParagraph(text));
     };
 
     /**
@@ -156,23 +175,14 @@ tie.factory('FeedbackObjectFactory', [
     };
 
     /**
-     * A getter for the _errorCategory property.
-     * This function should return a number that indicates what type of error
-     * corresponds to this feedback (if one occurs).
+     * A getter for the _feedbackCategory property.
+     * This function should return the category which corresponds to this
+     * feedback.
      *
-     * @returns {*|string}
+     * @returns {string}
      */
-    Feedback.prototype.getErrorCategory = function() {
-      return this._errorCategory;
-    };
-
-    /**
-     * A setter for the _errorCategory property.
-     *
-     * @param {string} category
-     */
-    Feedback.prototype.setErrorCategory = function(category) {
-      this._errorCategory = category;
+    Feedback.prototype.getFeedbackCategory = function() {
+      return this._feedbackCategory;
     };
 
     /**
@@ -214,7 +224,6 @@ tie.factory('FeedbackObjectFactory', [
       }
     };
 
-
     /**
      * Returns all of the feedback provided as a JSON dict, preserving type
      * and content information.
@@ -231,12 +240,14 @@ tie.factory('FeedbackObjectFactory', [
     /**
      * Returns a Feedback object based on the param passed in.
      *
+     * @param {string} feedbackCategory The category of the feedback. Must be
+     *    a valid entry in FEEDBACK_CATEGORIES.
      * @param {boolean} answerIsCorrect Indicates if the student's answer is
      *    correct or not.
      * @returns {Feedback}
      */
-    Feedback.create = function(answerIsCorrect) {
-      return new Feedback(answerIsCorrect);
+    Feedback.create = function(feedbackCategory, answerIsCorrect) {
+      return new Feedback(feedbackCategory, answerIsCorrect);
     };
 
     return Feedback;

client/question/domain/FeedbackObjectFactorySpec.js
@@ -23,33 +23,38 @@ describe('FeedbackObjectFactory', function() {
   var PARAGRAPH_TYPE_TEXT;
   var PARAGRAPH_TYPE_CODE;
   var PARAGRAPH_TYPE_SYNTAX_ERROR;
+  var FEEDBACK_CATEGORIES;
 
   beforeEach(module('tie'));
   beforeEach(inject(function($injector) {
     PARAGRAPH_TYPE_TEXT = $injector.get('PARAGRAPH_TYPE_TEXT');
     PARAGRAPH_TYPE_CODE = $injector.get('PARAGRAPH_TYPE_CODE');
     PARAGRAPH_TYPE_SYNTAX_ERROR = $injector.get('PARAGRAPH_TYPE_SYNTAX_ERROR');
-    FeedbackObjectFactory = $injector.get(
-      'FeedbackObjectFactory');
-    feedback = FeedbackObjectFactory.create(true);
+    FeedbackObjectFactory = $injector.get('FeedbackObjectFactory');
+    FEEDBACK_CATEGORIES = $injector.get('FEEDBACK_CATEGORIES');
+    feedback = FeedbackObjectFactory.create(
+      FEEDBACK_CATEGORIES.SUCCESSFUL, true);
   }));
 
   describe('isAnswerCorrect', function() {
-    it('should return whether or not answer is correct', function() {
+    it('should validate the feedback category', function() {
+      expect(function() {
+        FeedbackObjectFactory.create(
+          FEEDBACK_CATEGORIES.INVALID_CATEGORY, false);
+      }).toThrowError('Invalid feedback category: undefined');
+    });
+  });
+
+  describe('isAnswerCorrect', function() {
+    it('should return whether or not the answer is correct', function() {
       expect(feedback.isAnswerCorrect()).toBe(true);
     });
   });
 
-  describe('getErrorCategory', function() {
-    it('should return null', function() {
-      expect(feedback.getErrorCategory()).toBe(null);
-    });
-  });
-
-  describe('setErrorCategory', function() {
-    it('should set the _errorCategory property', function() {
-      feedback.setErrorCategory('ERROR OPERATOR');
-      expect(feedback._errorCategory).toBe('ERROR OPERATOR');
+  describe('getFeedbackCategory', function() {
+    it('should return the feedback category', function() {
+      expect(feedback.getFeedbackCategory()).toBe(
+        FEEDBACK_CATEGORIES.SUCCESSFUL);
     });
   });
 
@@ -65,7 +70,8 @@ describe('FeedbackObjectFactory', function() {
   describe('appendFeedback', function() {
     it('should append any feedback paragraphs from a given Feedback object',
       function() {
-        var feedbackToAppend = FeedbackObjectFactory.create(true);
+        var feedbackToAppend = FeedbackObjectFactory.create(
+          FEEDBACK_CATEGORIES.SUCCESSFUL, true);
         feedbackToAppend.appendTextParagraph('test1');
         feedbackToAppend.appendTextParagraph('test2');
         feedback.appendTextParagraph('testA');

commit 330c8baabb39fd49a9f4ec131a8ac5782706fb5c

Fix error introduced in commit 200c144 when checking whether code has changed.

client/question/services/FeedbackGeneratorService.js
@@ -175,7 +175,7 @@ tie.factory('FeedbackGeneratorService', [
       return (
         lastSnapshot !== null &&
         lastSnapshot.getCodeEvalResult() !== null &&
-        codeEvalResult.hasSamePreprocessedCodeAs(
+        !codeEvalResult.hasSamePreprocessedCodeAs(
           lastSnapshot.getCodeEvalResult()));
     };
 

client/question/services/SolutionHandlerServiceSpec.js
@@ -240,6 +240,49 @@ describe('SolutionHandlerService', function() {
           done();
         });
       });
+
+      it('should display a new message only if the code changes',
+        function(done) {
+          var studentCode1 = [
+            'def mockMainFunction(input):',
+            '    return True'
+          ].join('\n');
+          var studentCode2 = [
+            'def mockMainFunction(input):',
+            '    return True or True'
+          ].join('\n');
+
+          SolutionHandlerService.processSolutionAsync(
+            orderedTasks, starterCode, studentCode1,
+            auxiliaryCode, 'python'
+          ).then(function(feedback1) {
+            expect(feedback1.isAnswerCorrect()).toEqual(false);
+            expect(feedback1.getParagraphs()[0].getContent()).toEqual(
+               'Mock BuggyOutputTest Message One for task1');
+
+            SolutionHandlerService.processSolutionAsync(
+              orderedTasks, starterCode, studentCode1,
+              auxiliaryCode, 'python'
+            ).then(function(feedback2) {
+              expect(feedback2.isAnswerCorrect()).toEqual(false);
+              // The code has not changed, so the message stays the same.
+              expect(feedback2.getParagraphs()[0].getContent()).toEqual(
+                 'Mock BuggyOutputTest Message One for task1');
+
+              SolutionHandlerService.processSolutionAsync(
+                orderedTasks, starterCode, studentCode2,
+                auxiliaryCode, 'python'
+              ).then(function(feedback3) {
+                expect(feedback3.isAnswerCorrect()).toEqual(false);
+                // The code has changed, so the message changes.
+                expect(feedback3.getParagraphs()[0].getContent()).toEqual(
+                   'Mock BuggyOutputTest Message Two for task1');
+                done();
+              });
+            });
+          });
+        }
+      );
     });
 
     describe("prereqCheckFailures", function() {