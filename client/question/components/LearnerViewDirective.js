google/tie

commit 4cb578f6099fb19f17cc6219cd0834c42c466734

Increase contrast on some elements, add some accessibility attrs. (#492)
* Increase contrast on some elements, add some accessibility attrs.

* Address review comments; add aria-live attribute to feedback messages.

client/question/components/LearnerViewDirective.js
@@ -319,7 +319,7 @@ tie.directive('learnerView', [function() {
           padding: 10px;
         }
         .tie-question-title {
-          color: rgb(66, 133, 244);
+          color: #212121;
         }
         .tie-question-ui {
           vertical-align: top;
@@ -385,6 +385,10 @@ tie.directive('learnerView', [function() {
         .night-mode .tie-select-menu {
           background-color: #333a42;
           color: white;
+        }
+        .CodeMirror-linenumber {
+          /* Increase the contrast of the line numbers from the background. */
+          color: #424242;
         }
         .CodeMirror-line.tie-syntax-error-line {
           background: #FBC2C4;

client/question/components/SpeechBalloonsContainerDirective.js
@@ -27,7 +27,7 @@ tie.directive('speechBalloonsContainer', [function() {
           <div class="tie-dot tie-dot-2"></div>
           <div class="tie-dot tie-dot-3"></div>
         </div>
-        <div ng-repeat="balloon in ConversationLogDataService.data.speechBalloonList" track by $index>
+        <div ng-repeat="balloon in ConversationLogDataService.data.speechBalloonList track by $index" aria-live="assertive">
           <tie-speech-balloon-container>
             <div ng-if="balloon.isDisplayedOnLeft()">
               <tie-speech-balloon-left>

client/question/question.html
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html ng-app="tie">
+<html ng-app="tie" lang="en">
   <head>
+    <title>Technical Interview Exercises</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <link rel="stylesheet" href="../../third_party/codemirror-5.19.0/lib/codemirror.css">

commit 451d6664c8f0ac13dadc0806c42402ca364bde56

Add final sentence to congratulatory feedback. Fix regression caused by #492.

client/question/components/LearnerViewDirective.js
@@ -783,6 +783,8 @@ tie.directive('learnerView', [function() {
           congratulatoryFeedback.clear();
           congratulatoryFeedback.appendTextParagraph(
               "Good work! You've completed this question.");
+          congratulatoryFeedback.appendTextParagraph(
+              "(You can continue to submit additional answers, if you wish.)");
 
           ConversationLogDataService.addFeedbackBalloon(
             congratulatoryFeedback.getParagraphs());

client/question/components/SpeechBalloonsContainerDirective.js
@@ -27,7 +27,7 @@ tie.directive('speechBalloonsContainer', [function() {
           <div class="tie-dot tie-dot-2"></div>
           <div class="tie-dot tie-dot-3"></div>
         </div>
-        <div ng-repeat="balloon in ConversationLogDataService.data.speechBalloonList track by $index" aria-live="assertive">
+        <div ng-repeat="balloon in ConversationLogDataService.data.speechBalloonList" aria-live="assertive">
           <tie-speech-balloon-container>
             <div ng-if="balloon.isDisplayedOnLeft()">
               <tie-speech-balloon-left>