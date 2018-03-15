5f12fcca4c266019dff0d2fdd6fd30146b388ee2
Eric <eric.yurko@gmail.com>
2018-03-14 20:48:18 -0700

    Update question.js

--- client/question/question.js
+++ client/question/question.js
@@ -623,9 +623,9 @@ tie.constant('RUNTIME_ERROR_FEEDBACK_MESSAGES', {
     generateMessage: function() {
       return [
         "Unfortunately Python doesn't support directly assigning ",
-        "characters in a string. If you need to do so, try splitting the ",
-        "string, removing the old characters, and adding new ones. ",
-        "If you need a refresher on string manipulation, check out the primer."
+        "characters in a string. If you need to do so, try slicing the ",
+        "string and adding new characters instead of assigning them. ",
+        "If you need a refresher on slicing, check out the primer."
       ].join('');
     }
   }, {
