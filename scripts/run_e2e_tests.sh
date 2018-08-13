2860ce9ccd625066bda6178c1963856de65a2126
davyrisso-at-google <davyrisso@google.com>
2018-08-13 15:25:18 -0700

    Upgrades e2e tests to use async/await pattern

--- scripts/run_e2e_tests.sh
+++ scripts/run_e2e_tests.sh
@@ -16,17 +16,14 @@
 
 # TODO(feiluo): Need a way to control the log level to avoid thounsands of lines of log.
 
-set -e
-
-source $(dirname $0)/setup.sh || exit 1
+source $(dirname $0)/setup.sh
 
 # Install the following node modules if they aren't already installed.
-install_node_module protractor 5.1.1
+install_node_module protractor 5.4.0
 
 # Start up a Selenium Server
 ./node_modules/protractor/bin/webdriver-manager update
 ./node_modules/protractor/bin/webdriver-manager start &
 
 # Run the test
-./node_modules/protractor/bin/protractor ./protractor.conf.js
-
+./node_modules/protractor/bin/protractor ./test/e2e/protractor.conf.js
